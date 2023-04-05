1dcc50575c4225bfa497f99e9b5fabe3836f4862
Bob Merullo <bmerullo85@gmail.com>
2023-04-05 16:08:05 -0600

    Popular movie horizontal added

--- components/Header.js
+++ components/Header.js
@@ -8,39 +8,46 @@ import Button from "react-bootstrap/Button"
 const Header = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">MOOVIES</Navbar.Brand>
+
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Form className="d-flex">
                 <Form.Control
+                  size="sm"
                   type="search"
                   placeholder="Search"
                   className="me-2"
                   aria-label="Search"
                 />
-                <Button variant="outline-primary">Search</Button>
+                <Button variant="outline-primary" size="sm">
+                  Search
+                </Button>
               </Form>
             </Nav>
 
             <Nav>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
               <NavDropdown title="Menu" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+                <div className="nav-menu-container">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </div>
               </NavDropdown>
-              <Nav.Link href="#deets">Login</Nav.Link>
+              <Nav.Link href="#deets">Hi Bob Merullo</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,90 @@
 import Head from "next/head"
 import { Inter } from "next/font/google"
 import styles from "@/styles/Home.module.css"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
+import Carousel from "react-bootstrap/Carousel"
 
 const inter = Inter({ subsets: ["latin"] })
 
 const Home = ({ popularList }) => {
-  // console.log(popularList, "This is the list of movies")
+  console.log(popularList, "This is the list of movies")
   return (
     <>
       <Head>
         <title>MOOVIES</title>
         <meta
           name="description"
-          content="Community for people who love movies"
+          content="A Community for people who like movies"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1>MOOVIES</h1>
-        {popularList.results.map((movie, index) => {
-          return (
-            <div key={index}>
-              <p>{movie.title}</p>
-              <img
-                src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                alt="image"
-              />
-            </div>
-          )
-        })}
-        <p>A community for people who like movies</p>
+      <main>
+        <Container>
+          <h2 className="sub-title">Popular Right Now</h2>
+          <div className="background-box">
+            <section className="row-container">
+              {popularList.results.map((movie, index) => {
+                return (
+                  <Col>
+                    <div className="card-container">
+                      <Card style={{ width: "17rem" }}>
+                        <Card.Img
+                          className="rounded "
+                          variant="top"
+                          src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        />
+                      </Card>
+                    </div>
+                  </Col>
+                )
+              })}
+            </section>
+          </div>
+          <h2 className="sub-title">Popular Right Now</h2>
+          <div className="background-box">
+            <section className="row-container">
+              {popularList.results.map((movie, index) => {
+                return (
+                  <Col>
+                    <div className="card-container">
+                      <Card style={{ width: "17rem" }}>
+                        <Card.Img
+                          className="rounded "
+                          variant="top"
+                          src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        />
+                      </Card>
+                    </div>
+                  </Col>
+                )
+              })}
+            </section>
+          </div>
+          <h2 className="sub-title">Popular Right Now</h2>
+          <div className="background-box">
+            <section className="row-container">
+              {popularList.results.map((movie, index) => {
+                return (
+                  <Col>
+                    <div className="card-container">
+                      <Card style={{ width: "17rem" }}>
+                        <Card.Img
+                          className="rounded "
+                          variant="top"
+                          src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        />
+                      </Card>
+                    </div>
+                  </Col>
+                )
+              })}
+            </section>
+          </div>
+        </Container>
       </main>
     </>
   )

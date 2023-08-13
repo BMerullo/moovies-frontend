efb5922d705cc9a0d073801d6cdcd49e7b2fce6c
Bob Merullo <bmerullo85@gmail.com>
2023-08-13 17:21:35 -0600

    finished modal for login/registraion forms

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,33 +1,116 @@
-import React from "react"
+import React, { useState } from "react"
 import Modal from "react-bootstrap/Modal"
 import Button from "react-bootstrap/Button"
+import styles from "../styles/LoginModal.module.scss"
+import Form from "react-bootstrap/Form"
 
 const LoginModal = (props) => {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  )
+  const [register, setRegister] = useState(true)
+  if (register === true) {
+    return (
+      <Modal
+        {...props}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <div className={styles.container}>
+          <Modal.Header>
+            <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="password" />
+                <article className={styles.registerBtn}>
+                  <Button>Register</Button>
+                </article>
+                <article className={styles.register}>
+                  <p>
+                    Not a Member?{" "}
+                    <span
+                      id={styles.registerLink}
+                      onClick={() => setRegister(false)}
+                    >
+                      Register Here
+                    </span>
+                  </p>
+                </article>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={props.onHide}>X</Button>
+          </Modal.Footer>
+        </div>
+      </Modal>
+    )
+  } else {
+    return (
+      <Modal
+        {...props}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <div className={styles.container}>
+          <Modal.Header>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Register
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>email</Form.Label>
+                <Form.Control type="email" placeholder="email" />
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                ></Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="password" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" placeholder="confirm password" />
+              </Form.Group>
+              <article className={styles.registerBtn}>
+                <Button>Register</Button>
+              </article>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={props.onHide}>X</Button>
+          </Modal.Footer>
+        </div>
+      </Modal>
+    )
+  }
 }
 
 export default LoginModal
